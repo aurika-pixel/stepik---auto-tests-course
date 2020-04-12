96203b4b38b0c5ec20f15cec8400cee073d49d10
Juliia <aurikatu@mail.ru>
2020-04-12 21:06:53 +0300

    lesson4.3_step8

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,6 @@
 import math
-import time
 from selenium.webdriver.support import expected_conditions as EC
-
+from .locators import BasePageLocators
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -17,7 +16,7 @@ class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def is_element_present(self, how, what):
         try:
@@ -56,3 +55,10 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,21 +1,22 @@
 import time
-
 import pytest
+from basepage import BasePage
+
 from pages.product_page import ProductPage
 
 
-# @pytest.mark.parametrize('link_promo', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3",
-#                                         "?promo=offer4", "?promo=offer5", "?promo=offer6",
-#                                         pytest.param("?promo=offer7", marks=pytest.mark.xfail),
-#                                         "?promo=offer8", "?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser, link_promo):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link_promo}"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.click_basket_button()
-#     page.solve_quiz_and_get_code()
-#     page.check_book_name()
-#     page.check_basket_cost()
+@pytest.mark.parametrize('link_promo', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3",
+                                        "?promo=offer4", "?promo=offer5", "?promo=offer6",
+                                        pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                        "?promo=offer8", "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link_promo):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link_promo}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_basket_button()
+    page.solve_quiz_and_get_code()
+    page.check_book_name()
+    page.check_basket_cost()
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -41,3 +42,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.click_basket_button()
     time.sleep(2)
     page.should_not_be_success_message_is_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
